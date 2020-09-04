b1be468d66e01432c31fb15a33a96b6f06fff382
rohan <rohan@maheshs-mini.lan>
2020-09-03 19:09:52 -0700

    changedfilenames

--- app.py
+++ app.py
@@ -1,62 +1,11 @@
 import pymongo
-
 from flask import Flask, render_template, request, redirect
-app = Flask("register")
-myclient = pymongo.MongoClient("mongodb+srv://Rohan_Mahesh111:miniPEKKA5%@rohanmahesh.dhyd9.mongodb.net/sports?ssl=true&ssl_cert_reqs=CERT_NONE&retryWrites=true&w=majority")
-myDataBase = myclient["sports"]
-myCollection = myDataBase["soccer"]
-#reus = {"FirstName":"Marco","LastName":"Reus","Team":"Dortmund","jersey":11}
-#myCollection.insert_one(reus)
-#dblist = myclient.list_database_names()
-#print(dblist)
+app = Flask("People")
+myclient = pymongo.MongoClient("mongodb+srv://Rohan_Mahesh111:miniPEKKA5%@rohanmahesh.dhyd9.mongodb.net/people?ssl=true&ssl_cert_reqs=CERT_NONE&retryWrites=true&w=majority")
+myDataBase = myclient["people"]
+myCollection = myDataBase["UserRecords"]
+ActiveUser = ""
 
-#hazard = {"FirstName":"Eden","LastName":"Hazard","Team":"Madrid","jersey":11}
-#messi = {"FirstName":"Lionel","LastName":"Messi","Team":"Barcelona","jersey":10}
-#ronaldo = {"FirstName":"Cristiano","LastName":"Ronaldo","Team":"Juventus","jersey":7}
-
-# myCollection.insert_many([hazard,messi,ronaldo])
-
-# data = myCollection.find_one()
-# print(data)
-
-# da = myCollection.find()
-# for each in da:
-#     print(each)
-#
-# anot = myCollection.find({"jersey":11})
-# for each in anot:
-#     print(each)
-
-
-# anot = myCollection.find_one({"jersey":11})
-# print(anot)
-
-# anot = myCollection.find_one({"FirstName":"Cristiano"})
-# print(anot["LastName"])
-
-#myCollection.update_one({"FirstName":"Eden"},{"$set":{"jersey":7}})
-
-#myCollection.delete_one({"FirstName":"Marco"})
-
-#myCollection.update_one({"FirstName":"Cristiano"},{"$set":{"postion":"LW"}})
-
-#ano= myCollection.find_one({"FirstName":"Cristiano"})
-#print(ano["postion"])
-
-#ano= myCollection.find_one({"FirstName":"Lionel"})
-#if "postion" in ano:
-    #print(ano["postion"])
-#else:
-    #print("This person doesnt have a position")
-    #myCollection.update_one({"FirstName":"Lionel"},{"$set":{"postion":"RW"}})
-@app.route("/",methods=["GET","POST"])
-def login():
-    if request.method=="GET":
-        return render_template("login.html")
-    if request.method=="POST":
-        First = request.form["Firstname"]
-        Last = request.form["Lastname"]
-        return "hello"
 @app.route("/register",methods=["GET","POST"])
 def register():
     if request.method=="GET":
@@ -64,11 +13,88 @@ def register():
     if request.method=="POST":
         First = request.form["Firstname"]
         Last = request.form["Lastname"]
-        Team = request.form["Team"]
-        Jersey = request.form["Jersey"]
-        NewPlayer = {"FirstName":First,"LastName":Last,"Team":Team,"jersey":Jersey}
-        myCollection.insert_one(NewPlayer)
+        Username = request.form["Username"]
+        Email = request.form["Email"]
+        Password = request.form["Password"]
+        Confirm = request.form["Confirm"]
+        Birthday = request.form["Birthday"]
+        State = request.form["State"]
+        if Password == Confirm:
+            NewUser = {"FirstName":First,"LastName":Last,"Username":Username,"Email":Email,"Password":Password,"Birthday":Birthday,"State":State,"LoggedIn":False}
+            myCollection.insert_one(NewUser)
+        else:
+            print('Passwords do not match')
+            return redirect("/register")
+
         return redirect("/")
 
 
+@app.route("/",methods=["GET","POST"])
+def login():
+    global ActiveUser
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        username=request.form["UserName"]
+        Password = request.form["Password"]
+        person = myCollection.find_one({"Username":username})
+        print(person)
+        if person == None:
+            return redirect("/register")
+        else:
+            if Password == person["Password"]:
+                myCollection.update_one({"Username":username},{"$set":{"LoggedIn":True}})
+
+                return redirect("/home?username="+username)
+            else:
+                print("Incorrect password")
+                return redirect("/")
+
+@app.route("/home",methods=["GET","POST"])
+def home():
+
+    username = request.args.get("username")
+    if username is None:
+        return redirect("/")
+    print(username)
+    person = myCollection.find_one({"Username": username})
+    if request.method =="GET":
+        if "LoggedIn" in person:
+            if person["LoggedIn"]==True:
+                if "Status" in person:
+                    StatusList = person["Status"]
+                    return render_template("home.html", ActiveUser=username, Status=StatusList)
+                else:
+                    StatusList = "No Status Available"
+                    return render_template("home.html",ActiveUser=username,Status=StatusList)
+            else:
+                return redirect("/")
+        else:
+            myCollection.update_one({"Username": username}, {"$set": {"LoggedIn": False}})
+            print("Something went wrong")
+            return redirect("/")
+    if request.method =="POST":
+        newStatus=request.form["StatusBox"]
+        if "Status" in person:
+            StatusList = person["Status"]
+            StatusList.append(newStatus)
+            myCollection.update_one({"Username": username}, {"$set": {"Status": StatusList}})
+        else:
+            StatusList=[]
+            StatusList.append(newStatus)
+            myCollection.update_one({"Username": username}, {"$set": {"Status": StatusList}})
+        return redirect("/home?username="+username)
+
+
+
+@app.route("/logout")
+def logout():
+    username = (request.args.get("username"))
+    myCollection.update_one({"Username": username}, {"$set": {"LoggedIn": False}})
+    return redirect("/")
+
+
+
+
 app.run()
+
